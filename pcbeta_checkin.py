7425be2299f3ac7cb395d0c2940ca0ef99284ee8
yuan-xinjie <yuanxinjie@gmail.com>
2023-04-23 23:17:18 +0800

    update

--- pcbeta_checkin.py
+++ pcbeta_checkin.py
@@ -88,10 +88,10 @@ def getTaskID():
 
 def pcbetaReply():
     taskName = "回帖打卡福利"
-    # 获取任务id
-    global idd
-    idd = getTaskID()
     if taskName in newTaskRes:
+		# 获取任务id
+		global idd
+		idd = getTaskID()
         # 申请回帖打卡任务
         reRes = requests.get(url=f"https://i.pcbeta.com/home.php?mod=task&do=apply&id={idd}", headers=pcHeaders)
         if "任务申请成功" in reRes.text:
