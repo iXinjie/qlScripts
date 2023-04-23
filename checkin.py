bb6153a6efea83c4b44ad8d8c3b8057c8649f7aa
Xinjie <74199447+iXinjie@users.noreply.github.com>
2023-04-24 00:00:00 +0800

    Delete checkin.py

--- checkin.py
+++
@@ -1,9 +0,0 @@
-"""
-cron: 0 0 8 ? * *
-new Env('远景论坛签到');
-"""
-import checkin_pcbeta
-pcBetaCheckinlog = f"远景论坛签到： {checkin_pcbeta.pcbetaCheckin()}"
-print(pcBetaCheckinlog)
-pcBetaReplyLog = f"远景论坛打卡： {checkin_pcbeta.pcbetaReply()}"
-print(pcBetaReplyLog)
