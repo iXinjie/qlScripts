5dbe9e94d1edfca0d0193c5c304905bff84086d6
yuan-xinjie <yuanxinjie@gmail.com>
2023-05-23 18:54:12 +0800

    Update mt_checkin.py

--- mt_checkin.py
+++ mt_checkin.py
@@ -97,8 +97,10 @@ if __name__ == "__main__":
                 msg = '账号登录失败'
                 print(f'{username}\n{password}')
             else:
-                msg = checkinfo() + checkin()
-                # 青龙通知推送
+				c = checkin()
+				info = checkinfo()
+                msg = c + info
+        # 青龙通知推送
         send('MT论坛签到',msg)
     else:
         print('未添加"MT_BBS"变量，退出')
